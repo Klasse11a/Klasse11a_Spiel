a1601e3d2957e436ce5598ffbcf9399743d42faf
Wiener234 <wiemernils@gmail.com>
2020-12-10 09:31:47 +0100

    Camera
    camera geht nicht durch boden

---
+++ Klasse11a/Assets/Scripts/turnoffcamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnoffcamera : MonoBehaviour
+{
+    public GameObject dollycam1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Dollycam1());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Dollycam1()
+    {
+        yield return new WaitForSecondsRealtime(25.26f);
+        dollycam1.SetActive(false);
+    }
+}
